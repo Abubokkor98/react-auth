8ce1a72e0db48a249f44b02ea2456e72673b4b4e
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-12 18:35:51 +0600

    Create Login and Registration Form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-3xl font-bold">Login now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body" onSubmit={handleLogin}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+
+          <p className="p-4 mb-4">New to this website? Please <span className="underline text-red-500"><Link to={'/register'}>Register first</Link></span></p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Register() {
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
+
   return (
-    <div>Register</div>
+    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content flex-col">
+      <div className="text-center lg:text-left">
+        <h1 className="text-3xl font-bold">Register now!</h1>
+      </div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body" onSubmit={handleRegister}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="name"
+              name="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+
+        <p className="p-4 mb-4">Already Registered? Please <span className="underline text-red-500"><Link to={'/login'}>Login</Link></span></p>
+      </div>
+    </div>
+  </div>
   )
 }
