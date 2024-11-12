45def74071353000f995b3e7b91d9487e80da470
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-12 19:40:18 +0600

    Create user using auth provider context

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
 
 export default function Register() {
+    // access context API from AuthProvider Component
+    const {createUser} = useContext(AuthContext);
 
     const handleRegister = e => {
         e.preventDefault();
@@ -9,6 +12,15 @@ export default function Register() {
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(name, email, password);
+
+        // create user
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user);
+        })
+        .catch(error=>{
+            console.log("ERROR",error);
+        })     
     }
 
   return (
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,16 +1,24 @@
 import React, { createContext } from "react";
+import { auth } from "../firebase.init";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
+  const name = "Axd Ab";
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //this is context API
   const authInfo = {
-    name: "Axd Ab",
+    name,
+    createUser,
   };
 
   return (
-    <AuthContext.Provider value={authInfo}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 }
 
